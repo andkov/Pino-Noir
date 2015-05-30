c8ba1e50c92de860a462760cf8ab982a41aa142a
Andrey Koval <koval.andrey@gmail.com>
2015-05-30 16:45:22 -0700

    ready

--- 0_enter_data.R
+++ 0_enter_data.R
@@ -2,4 +2,170 @@ options(width=160)
 rm(list=ls())
 cat("\f")
 
+## @knitr load_packages
+library(dplyr)
+
+## @knitr define_labels
+fruit <- c("Plum", "Cherry","Currant","Tomato", "Raspberry", "Strawberry")
+oak <- c("Oak","Vanilla", "Smoke", "Pepper", "Coconut", "Cinnamon")
+complex <- c("Earth", "Coffee", "Leather", "Tobacco","Chocolate","Mushroom")
+
+labels <- c(fruit, oak, complex)
+
+## @knitr define_wines
+wine1 <- list("Name"= "Innocent Bystander",
+              "Region"="Australia",
+              "Year"="2006"); 
+wine2 <- list("Name"= "Lucia Highlands",
+              "Region"="California",
+              "Year"="2009"); 
+wine3 <- list("Name"= "Kris",
+              "Region"="New Zealand",
+              "Year"="2007"); 
+wine4 <- list("Name"= "Innocent Bystander II",
+              "Region"="Australia II",
+              "Year"="2002"); 
+wine5 <- list("Name"= "Lucia Highlands II",
+              "Region"="California II",
+              "Year"="2001"); 
+wine6 <- list("Name"= "Kris II",
+              "Region"="New Zealand II",
+              "Year"="2003"); 
+
+wines <- list(wine1, wine2, wine3)
+names(wines) <- paste0("wine",1:length(wines))
+
+data <- list()
+
+## @knitr define_compiling_function
+compile.data <- function(wineID, personID){
+  hat <- list("wine"=wine,"PersonID"=person)
+    value <- c(  aroma_fruit,   aroma_oak,   aroma_complex,
+             flavour_fruit, flavour_oak, flavour_complex)
+  label <- c(labels, labels)
+  sense <- c(rep("aroma", length(labels)), rep("flavour", length(labels)))
+  group <- c( rep("fruit", length(fruit)),rep("oak", length(oak)),rep("complex", length(complex)) )
+  ds <- as.data.frame(cbind(value, label))
+  ds$person <- hat[["PersonID"]]
+  ds$wine <- hat[["wine"]][["Name"]]
+  ds$region <- hat[["wine"]][["Region"]]
+  ds$year <- hat[["wine"]][["Year"]]
+  ds$sense <- sense
+  ds$group <- group
+  ds$like <- likability
+  ds <- ds %>% dplyr::select(person, wine, region, year, sense, group, label, value, like)
+  # data[[paste0( hat[["wine"]][["Name"]]," by ", hat[["PersonID"]])]]
+  return(ds)
+}
+
+
+dtos <- list()
+## @knitr enter_data
+
+# #################################
+# wine <- "wine"
+# person <- "person"
+# aroma_fruit     <- c()
+# aroma_oak       <- c()
+# aroma_complex   <- c()
+# flavour_fruit   <- c()
+# flavour_oak     <- c()
+# flavour_complex <- c()
+# dtos[[wine]][[person]] <- compile.data(wine,person)
+
+################################# 
+wine <- wine1
+person <- "Ken"
+aroma_fruit     <- c(0,1,0,1,1,0)
+aroma_oak       <- c(1,0,1,1,1,0)
+aroma_complex   <- c(1,0,0,0,0,1)
+flavour_fruit   <- c(1,1,0,0,0,0)
+flavour_oak     <- c(0,0,0,1,0,0)
+flavour_complex <- c(1,1,1,1,0,0)
+likability <- 5
+dtos[[ paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+
+################################# 
+wine <- wine2
+person <- "Bob"
+aroma_fruit     <- c(0,1,1,1,0,1)
+aroma_oak       <- c(0,0,0,1,1,1)
+aroma_complex   <- c(1,0,1,0,1,0)
+flavour_fruit   <- c(1,1,1,1,1,1)
+flavour_oak     <- c(0,0,0,0,1,1)
+flavour_complex <- c(0,1,0,1,1,1)
+likability <- 3
+dtos[[ paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+################################# 
+wine <- wine3
+person <- "Jake"
+aroma_fruit     <- c(0,1,1,0,0,0)
+aroma_oak       <- c(1,0,0,1,0,1)
+aroma_complex   <- c(0,1,0,1,0,0)
+flavour_fruit   <- c(0,0,0,0,1,0)
+flavour_oak     <- c(0,0,0,1,0,1)
+flavour_complex <- c(0,0,0,1,0,1)
+likability <- 7
+dtos[[ paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+# ################################# 
+wine <- wine1
+person <- "Mike"
+aroma_fruit     <- c(1,1,1,0,1,0)
+aroma_oak       <- c(0,1,0,1,1,1)
+aroma_complex   <- c(0,1,0,0,0,0)
+flavour_fruit   <- c(1,0,1,0,0,0)
+flavour_oak     <- c(0,0,0,0,1,1)
+flavour_complex <- c(1,1,1,0,1,0)
+likability <- 1
+dtos[[paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+################################# 
+wine <- wine4
+person <- "Jake"
+aroma_fruit     <- c(1,1,1,0,1,0)
+aroma_oak       <- c(0,1,0,1,0,1)
+aroma_complex   <- c(0,1,0,1,0,0)
+flavour_fruit   <- c(1,0,1,1,1,0)
+flavour_oak     <- c(0,0,1,0,0,1)
+flavour_complex <- c(1,1,1,0,1,0)
+likability <- 4
+dtos[[paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+################################# 
+wine <- wine5
+person <- "John"
+aroma_fruit     <- c(1,1,1,0,0,1)
+aroma_oak       <- c(0,1,0,1,0,1)
+aroma_complex   <- c(0,1,0,0,0,0)
+flavour_fruit   <- c(1,0,1,0,1,0)
+flavour_oak     <- c(0,0,0,0,1,0)
+flavour_complex <- c(1,0,0,0,1,0)
+likability <- 2
+dtos[[paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+################################# 
+wine <- wine6
+person <- "Trevor"
+aroma_fruit     <- c(1,1,1,0,1,0)
+aroma_oak       <- c(0,1,0,1,1,1)
+aroma_complex   <- c(0,1,0,1,0,0)
+flavour_fruit   <- c(1,0,1,0,0,0)
+flavour_oak     <- c(0,1,0,0,1,1)
+flavour_complex <- c(1,0,1,0,0,0)
+likability <- 6
+dtos[[paste0(wine[["Name"]],"_",person)]]<- compile.data(wine,person)
+
+
+
+
+## @knitr produce_final_data
+data <- plyr::ldply(dtos, data.frame)
+rm( list = setdiff(ls(),c("data","labels")))
+saveRDS(data,"data.rds")
+
+
+
 
---
+++ 1_graph_data.R
@@ -0,0 +1,141 @@
+options(width=160)
+rm( list = setdiff(ls(),c("data","labels")))
+cat("\f")
+
+## @knitr load_packages
+library(dplyr)
+library(ggplot2)
+
+## @knitr load_data
+# data <- readRDS("./data.rds")
+source("./0_enter_data.R")
+
+## @knitr tweak_data
+
+data <- data
+str(data)
+data$value <- as.numeric(data$value)-1
+data$valueTF <- as.logical(data$value)
+head(data)
+
+
+
+# res$label2
+# print(res, n=nrow(res))
+
+## @knitr define_themes
+baseSize <- 12
+theme1 <- ggplot2::theme_bw(base_size=baseSize) +
+  ggplot2::theme(title=ggplot2::element_text(colour="gray20",size = baseSize+1)) +
+  ggplot2::theme(axis.text=ggplot2::element_text(colour="gray40", size=baseSize-2)) +
+  ggplot2::theme(axis.title=ggplot2::element_text(colour="gray40")) +
+  ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80")) +
+  ggplot2::theme(axis.ticks.length = grid::unit(0, "cm")) +
+  ggplot2::theme(text = element_text(size =baseSize+7)) 
+
+## @knitr counts
+ds <- data %>%
+  dplyr::group_by(wine) %>%
+  summarize(count=n_distinct(.id))
+print(ds, n=nrow(ds))
+
+ds <- data %>%
+  dplyr::group_by(wine, person) %>%
+  summarize(count=n_distinct(.id))
+t <- table(ds$wine, ds$person)
+t[t==0] <- "."
+t
+
+
+
+## @knitr likability
+ds <- data %>% dplyr::select(person, wine, region, year, like) %>%
+  dplyr::group_by(person, wine, region, year) %>%
+  dplyr::summarize(like=mean(like)) %>%
+  dplyr::arrange(person,wine)
+ds <- ds %>% dplyr::group_by(wine, region) %>%
+  dplyr::summarize(like=mean(like)) 
+
+
+g <- ggplot2::ggplot(ds, aes(x=wine,y=like, fill=region))
+g <- g + geom_bar(stat="identity")
+# g <- g + scale_y_discrete(limits=rev(labels))
+g <- g + labs(title="Average likability across raters", x="Wine", y="Average likability")
+# g <- g + facet_grid(.~sense)
+# g <- g + scale_fill_gradient(low="white", high="red")
+g <- g + geom_text(aes(label=like), vjust=1.5 )
+g <- g + theme1
+# g <- g + theme(axis.title.y = element_blank())
+g
+
+
+
+## @knitr summary_heat
+ds <- data
+res <- ds %>% dplyr::group_by(wine,label, sense) %>%
+  dplyr::summarize(score=mean(value))
+##
+
+g <- ggplot2::ggplot(res, aes(x=wine,y=label, fill=score))
+g <- g + geom_tile()
+g <- g + scale_y_discrete(limits=rev(labels))
+g <- g + labs(title="Summary scores across raters", x="Wine")
+g <- g + facet_grid(.~sense)
+g <- g + scale_fill_gradient(low="white", high="red")
+g <- g + geom_text(aes(label=score))
+g <- g + theme1
+g <- g + theme(axis.title.y = element_blank())
+g
+
+## @knitr aroma_heat
+ds <- data[data$sense=="aroma",]
+res <- ds %>%  
+  dplyr::group_by(wine,label) %>%
+  dplyr::summarize(score=mean(value))
+g <- ggplot2::ggplot(res, aes(x=wine,y=label, fill=score))
+g <- g + geom_tile()
+g <- g + scale_y_discrete(limits=rev(labels))
+g <- g + labs(title="Summary scores across raters", x="Wine")
+# g <- g + facet_grid(.~sense)
+g <- g + scale_fill_gradient(low="white", high="red")
+g <- g + geom_text(aes(label=score))
+g <- g + theme1
+g <- g + theme(axis.title.y = element_blank())
+g
+
+## @knitr flavour_heat
+ds <- data[data$sense=="flavour",]
+res <- ds %>%  
+  dplyr::group_by(wine,label) %>%
+  dplyr::summarize(score=mean(value))
+g <- ggplot2::ggplot(res, aes(x=wine,y=label, fill=score))
+g <- g + geom_tile()
+g <- g + scale_y_discrete(limits=rev(labels))
+g <- g + labs(title="Summary scores across raters", x="Wine")
+# g <- g + facet_grid(.~sense)
+g <- g + scale_fill_gradient(low="white", high="red")
+g <- g + geom_text(aes(label=score))
+g <- g + theme1
+g <- g + theme(axis.title.y = element_blank())
+g
+
+## @knitr both_heat
+ds <- data
+res <- ds %>%  
+  dplyr::group_by(wine,label) %>%
+  dplyr::summarize(score=mean(value))
+g <- ggplot2::ggplot(res, aes(x=wine,y=label, fill=score))
+g <- g + geom_tile()
+g <- g + scale_y_discrete(limits=rev(labels))
+g <- g + labs(title="Summary scores across raters", x="Wine")
+# g <- g + facet_grid(.~sense)
+g <- g + scale_fill_gradient(low="white", high="red")
+g <- g + geom_text(aes(label=score))
+g <- g + theme1
+g <- g + theme(axis.title.y = element_blank())
+g
+# table(ds$valueTF, ds$wine)
+
+
+
+
